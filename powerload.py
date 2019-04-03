47a5c522301de6b6df636826c23f3b2b31983c5b
arotker45 <46360914+arotker45@users.noreply.github.com>
2019-04-03 17:01:29 -0400

    Add files via upload

---
+++ powerload.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+This class represents a location that is drawing electrical power off the grid, and can be used to represent a single home, an
+industrial facility, or even and entire town or city; it is hooked up to one or more point reactor modules, which provide it with power
+in exchange for . It projects its own future power needs based on a function provided during
+initialization, and can also experience random spikes in energy need as well.
+"""
+
+class PowerLoad():
+    def __init__(self, base_function, spike_function)
+    
+    
+
